4c23fd9d9a27afdda22bd9c0ffa5a105e4b50650
wungjyan <1170017412@qq.com>
2023-04-11 10:46:25 +0800

    处理多对一关系，多个effeci对应一个响应数据

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,5 +1,10 @@
+import { Dep, createDep } from './dep'
+
 // Map 的 key 是 target 对象的某个属性，value 是此属性绑定的 activeEffect
-type KeyToMap = Map<any, ReactiveEffect>
+// type KeyToMap = Map<any, ReactiveEffect>
+
+// 一个 key 对应多个 effect 时，value 是一个多 activeEffect 的 Set 类型
+type KeyToMap = Map<any, Dep>
 
 // 用于缓存数据依赖关系
 // key 是传入 reactive 的原对象（target），value 是一个 Map
@@ -15,7 +20,7 @@ export function effect<T = any>(fn: () => T) {
 // 当前激活的 effect
 let activeEffect: ReactiveEffect | undefined
 
-class ReactiveEffect<T = any> {
+export class ReactiveEffect<T = any> {
   constructor(public fn: () => T) {}
 
   run() {
@@ -32,7 +37,16 @@ export function track(target: object, key: unknown) {
   if (!depsMap) {
     targetMap.set(target, (depsMap = new Map()))
   }
-  depsMap.set(key, activeEffect)
+  // depsMap.set(key, activeEffect) // 一个 key 对应一个 effect 时，value 是单一的 activeEffect
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+  trackEffects(dep)
+}
+
+export function trackEffects(dep: Dep) {
+  dep.add(activeEffect!)
 }
 
 // 触发依赖（set时）
@@ -42,10 +56,25 @@ export function trigger(target: object, key: unknown, newValue: unknown) {
     return
   }
 
-  const effect = depsMap.get(key) as ReactiveEffect
-  if (!effect) {
+  // 处理一个 key 对应多个 effect
+  const dep: Dep | undefined = depsMap.get(key)
+  if (!dep) {
     return
   }
+  triggerEffects(dep)
 
-  effect.fn()
+  // 一个 key 只对应一个 effect 时候
+  // const effect = depsMap.get(key) as ReactiveEffect
+  // if (!effect) {
+  //   return
+  // }
+
+  // effect.fn()
+}
+
+function triggerEffects(dep: Dep) {
+  const effects = Array.isArray(dep) ? dep : [...dep]
+  for (const effect of effects) {
+    effect.fn()
+  }
 }
